d462093ac861428c8a4a9643e8a20fb1fc08ac44
Tautvydas <t.steponavicius@icloud.com>
2025-01-31 14:57:20 +0200

    Solved task 5

--- tests/task-5-perfect-circle.spec.ts
+++ tests/task-5-perfect-circle.spec.ts
@@ -30,7 +30,8 @@ function getCirclePoints(
 
 async function getSvgCenterInPageCoords(
   page: Page,
-  svgSelector: string
+  svgSelector: string,
+  radiusPercentage = 0.75
 ): Promise<[Coordinate, number]> {
   const svgBoundingBox = await page.locator(svgSelector).boundingBox();
   if (!svgBoundingBox) throw new Error("SVG element not found");
@@ -40,7 +41,7 @@ async function getSvgCenterInPageCoords(
       x: svgBoundingBox.x + svgBoundingBox.width / 2,
       y: svgBoundingBox.y + svgBoundingBox.height / 2,
     },
-    (svgBoundingBox.width / 2) * 0.7,
+    Math.floor(svgBoundingBox.width / 2) * radiusPercentage,
   ];
 }
 
@@ -57,7 +58,26 @@ async function drawCircle(page: Page, points: Coordinate[]) {
 }
 
 test.describe("Task 5: Perfect circle", async () => {
-  test("1. Get the worst possible score - 3 points", async ({ page }) => {});
+  test("1. Get the worst possible score - 3 points", async ({ page }) => {
+    // Open the page and press the "Go" button
+    await page.goto("https://neal.fun/perfect-circle/");
+    await page.locator("button.fc-cta-consent").click();
+    await page.getByRole("button", { name: "Go" }).click();
+
+    // Get center point
+    const radiusPercentage = 0.75;
+    const [centerPoint, radius] = await getSvgCenterInPageCoords(page, "svg", radiusPercentage);
+
+    // Get all points on the circle
+    const points = [
+      { x: centerPoint.x + radius * 0.3, y: centerPoint.y },
+      ...getCirclePoints(centerPoint, radius),
+    ];
+
+    // Draw circle from points
+    drawCircle(page, points);
+    await page.waitForTimeout(10000);
+  });
 
   test("2. Get the best possible score among all teams - 3 points", async ({ page }) => {
     // Open the page and press the "Go" button
@@ -66,14 +86,34 @@ test.describe("Task 5: Perfect circle", async () => {
     await page.getByRole("button", { name: "Go" }).click();
 
     // Get center point
-    const [centerPoint, radius] = await getSvgCenterInPageCoords(page, "svg");
+    const radiusPercentage = 0.75;
+    const [centerPoint, radius] = await getSvgCenterInPageCoords(page, "svg", radiusPercentage);
 
     // Get all points on the circle
-    const points = getCirclePoints(centerPoint, radius);
+    const points = getCirclePoints(centerPoint, radius, radius * 1.2);
 
     // Draw circle from points
     drawCircle(page, points);
     await page.waitForTimeout(10000);
   });
-  test("3. Score exactly 66.6% - 4 points", async ({ page }) => {});
+  test("3. Score exactly 66.6% - 4 points", async ({ page }) => {
+    // Open the page and press the "Go" button
+    await page.goto("https://neal.fun/perfect-circle/");
+    await page.locator("button.fc-cta-consent").click();
+    await page.getByRole("button", { name: "Go" }).click();
+
+    // Get center point
+    const radiusPercentage = 0.76;
+    const [centerPoint, radius] = await getSvgCenterInPageCoords(page, "svg", radiusPercentage);
+
+    // Get all points on the circle
+    const points = [
+      { x: centerPoint.x + radius * 0.75, y: centerPoint.y },
+      ...getCirclePoints(centerPoint, radius),
+    ];
+
+    // Draw circle from points
+    drawCircle(page, points);
+    await page.waitForTimeout(10000);
+  });
 });
